9936c3621971f79f21b48c0bf8ba4d185e7f5f42
leesoyul <tndus1303@gmail.com>
2023-07-13 19:23:25 +0900

    add Main page item contents

--- 02/src/App.js
+++ 02/src/App.js
@@ -4,33 +4,22 @@ import Product from "./pages/Product";
 import Main from "./pages/Main";
 import Products from "./pages/Products";
 import Layout from "./common/Layout";
-import { Link } from "react-router-dom";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <>
-      {/* Routes하위에 경로에 따라 의 Route에 element전달한 구성요소 */}
       <Routes>
         <Route element={<Layout />}>
-          {/* 특정한 경로(path)와 일치할 때 해당 라우터가 렌더링된다. */}
           <Route path="/" element={<Main />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
           <Route path="/products" element={<Products />} />
-          {/* URL Parameter 작성방법 --> : <아무 이름> */}
           <Route path="/products/:id" element={<Product />} />
-          {/* "*"기호는 모든 경로와 일치하므로, 어떤 경로에서도 해당 라우터가 렌더링된다. */}
-          {/* 주로 404 에러 페이지나 기본 경로(default path)로 사용된다. */}
-          <Route path="/login" element={<div>로그인 페이지입니다.</div>} />
-          <Route path="/signup" element={<div>회원가입 페이지입니다.</div>} />
+          <Route path="*" element={<NotFound />} />
         </Route>
-        <Route
-          path="*"
-          element={
-            <>
-              <div>없는 페이지 입니다.</div>
-              <Link to="/">홈으로</Link>
-            </>
-          }
-        />
       </Routes>
     </>
   );
--- 02/src/pages/Main.jsx
+++ 02/src/pages/Main.jsx
@@ -1,7 +1,47 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { nanoid } from "nanoid";
+import { styled } from "styled-components";
 
 export default function Main() {
+  const [items, setItems] = useState([
+    {
+      id: nanoid(),
+      name: "멋진 바지",
+      price: 20000,
+      option: [
+        { value: 1, name: "28" },
+        { value: 2, name: "30" },
+        { value: 3, name: "32" },
+      ],
+      like: 100,
+    },
+    {
+      id: nanoid(),
+      name: "멋진 셔츠",
+      price: 10000,
+      option: [
+        { value: 1, name: "small" },
+        { value: 2, name: "medium" },
+        { value: 3, name: "large" },
+      ],
+      like: 200,
+    },
+    {
+      id: nanoid(),
+      name: "멋진 신발",
+      price: 30000,
+      option: [
+        { value: 1, name: "230" },
+        { value: 2, name: "240" },
+        { value: 3, name: "250" },
+        { value: 4, name: "260" },
+        { value: 5, name: "270" },
+      ],
+      like: 300,
+    },
+  ]);
   return (
     <>
       {/* main */}
@@ -29,6 +69,7 @@ export default function Main() {
           <h2>🔥 여름 추천템 🔥</h2>
           {/* react-router-dom의 a태그 = Link */}
           <Link to="/products">더보기</Link>
+          {/* map함수를 이용해 각 카드 렌더링 */}
           <div
             style={{
               display: "flex",
@@ -36,33 +77,12 @@ export default function Main() {
               gap: "24px",
             }}
           >
-            <div
-              style={{
-                width: "200px",
-                height: "240px",
-                backgroundColor: "#068FFF",
-              }}
-            >
-              상품1
-            </div>
-            <div
-              style={{
-                width: "200px",
-                height: "240px",
-                backgroundColor: "#068FFF",
-              }}
-            >
-              상품2
-            </div>
-            <div
-              style={{
-                width: "200px",
-                height: "240px",
-                backgroundColor: "#068FFF",
-              }}
-            >
-              상품3
-            </div>
+            {items.map((item) => (
+              <StCard key={item.id}>
+                <div>{item.name}</div>
+                <div>{item.price}</div>
+              </StCard>
+            ))}
           </div>
         </section>
         {/* 추가적인 데이터 */}
@@ -80,36 +100,24 @@ export default function Main() {
               gap: "24px",
             }}
           >
-            <div
-              style={{
-                width: "200px",
-                height: "240px",
-                backgroundColor: "#EEEEEE",
-              }}
-            >
-              상품1
-            </div>
-            <div
-              style={{
-                width: "200px",
-                height: "240px",
-                backgroundColor: "#EEEEEE",
-              }}
-            >
-              상품2
-            </div>
-            <div
-              style={{
-                width: "200px",
-                height: "240px",
-                backgroundColor: "#EEEEEE",
-              }}
-            >
-              상품3
-            </div>
+            <StSaleCard>상품1</StSaleCard>
+            <StSaleCard>상품2</StSaleCard>
+            <StSaleCard>상품3</StSaleCard>
           </div>
         </section>
       </main>
     </>
   );
 }
+
+const StCard = styled.div`
+  width: 200px;
+  height: 240px;
+  background-color: #068fff;
+`;
+
+const StSaleCard = styled.div`
+  width: 200px;
+  height: 240px;
+  background-color: white;
+`;
